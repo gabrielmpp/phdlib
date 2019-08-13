52ea6d6ec87b0bbf4c55cc5c5f973143e5b895ec
gabriel <gabrielmpp2@gmail.com>
2019-08-13 18:56:26 +0100

    update

--- convlib/run_jasmin.py
+++ convlib/run_jasmin.py
@@ -6,13 +6,12 @@ if __name__ == '__main__':
     script_paths = ['/home/users/gmpp/phdlib/convlib/classifier.py']
 
     for script_path in script_paths:
-        for year in years:
-            subprocess.call(['bsub',
-            '-o',logs_path+'%J.out',
-            '-e',logs_path+'%J.err',
-            '-W','12:00',
-            '-R','rusage[mem=15000]',
-            '-M','15000',
-            '-n','8',
-            '-q','par-single',
-             python,script_path, 'jasmin'])
+         subprocess.call(['bsub',
+         '-o',logs_path+'%J.out',
+         '-e',logs_path+'%J.err',
+         '-W','12:00',
+         '-R','rusage[mem=15000]',
+         '-M','15000',
+         '-n','8',
+         '-q','par-single',
+         python,script_path, 'jasmin'])
