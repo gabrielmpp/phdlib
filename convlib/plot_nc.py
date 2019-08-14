51251af6a10cb8a8a645c6b5b4ec9e4b20872669
gabriel <gabrielmpp2@gmail.com>
2019-08-14 13:12:01 +0100

    update

--- convlib/plot_nc.py
+++ convlib/plot_nc.py
@@ -7,7 +7,6 @@ array = xr.open_dataarray(filepath)
 
 for time in array.time.values:
     plt.figure(figsize=[13,13])
-    array = array ** 0.5
-    array.sel(time=time).plot.contourf(vmin=0,vmax=1,cmap = 'nipy_spectral')
+    array.sel(time=time).plot.contourf(vmin=0,levels=40,vmax=1,cmap = 'nipy_spectral')
     plt.savefig(f'./tempfigs/SL{time}.png')
     plt.close()
