4e183dc36cef364a51798dffa3fa6294b3a29a65
gabriel <gabrielmpp2@gmail.com>
2019-08-14 13:22:51 +0100

    update

--- convlib/plot_nc.py
+++ convlib/plot_nc.py
@@ -9,7 +9,7 @@ array = xr.open_dataarray(filepath)
 
 for time in array.time.values:
     f, axarr = plt.subplots(1, 1, figsize=(14, 14), subplot_kw={'projection': ccrs.PlateCarree()})
-    array.sel(time=time).plot.contourf(vmin=0,levels=40,vmax=1,cmap = 'nipy_spectral',transform=ccrs.PlateCarree(), ax=axarr[0])
+    array.sel(time=time).plot.contourf(vmin=0,levels=40,vmax=0.6,cmap = 'nipy_spectral',transform=ccrs.PlateCarree(), ax=axarr[0])
     axarr[0].coastlines()
     axarr[0].add_feature(states_provinces, edgecolor='gray')
     plt.savefig(f'./tempfigs/SL{time}.png')
