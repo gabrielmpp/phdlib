85060120fe4e021b99eac28c66c26c05d0b6adb1
Gabriel Martins Palma Perez <gmpp@jasmin-sci4.ceda.ac.uk>
2019-08-19 14:34:12 +0100

    update

--- download_era/call_download.py
+++ download_era/call_download.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == '__main__':
     logs_path = '/home/users/gmpp/logs/'
     python = '/home/users/gmpp/miniconda2/envs/phd/bin/python'
-    script_paths = ['/home/users/gmpp/phdlib/download_era/download_era_vimae.py']
+    script_paths = ['/home/users/gmpp/phdlib/download_era/download_era_viman.py']
     years = np.arange(1979,2010,1)
     for script_path in script_paths:
         for year in years:
