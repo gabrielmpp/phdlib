cb64a773a07ee9427ad885d793c264d0b4d7e198
gabriel <gabrielmpp2@gmail.com>
2019-08-27 21:09:26 +0100

    update

--- convlib/classifier.py
+++ convlib/classifier.py
@@ -114,8 +114,8 @@ if __name__ == '__main__':
     else:
         outpath = 'data/'
 
-    classified_array1 = classifier(config, method ='lagrangian')
-    classified_array2 = classifier(config, method = 'conv')
+    #classified_array1 = classifier(config, method='lagrangian', lcs_type='attracting')
+    classified_array2 = classifier(config, method='conv')
 
     classified_array1.to_netcdf(f'{outpath}SL_attracting.nc')
 
