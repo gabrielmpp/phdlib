b3f5acbb457472366fb740335faf5aedd0574201
gabriel <gabrielmpp2@gmail.com>
2019-09-02 00:27:19 +0100

    update

--- convlib/classifier.py
+++ convlib/classifier.py
@@ -114,8 +114,8 @@ class Classifier:
         v.name = 'v'
         ds = xr.merge([u, v])
         print(ds)
-        lcs = LCS(lcs_type=lcs_type, timestep=timestep, dataarray_template=u[0, :, :])
-        eigenvalues = xr.apply_ufunc(lambda x, y: lcs(u=x, v=y), u, v, dask='parallelized')
+        lcs = LCS(lcs_type=lcs_type, timestep=timestep, dataarray_template=u.isel(time=0).drop('time'))
+        eigenvalues = xr.apply_ufunc(lambda x, y: lcs(u=x, v=y), u.groupby('time'), v.groupby('time'), dask='parallelized')
         #eigenvalues = ds.groupby('time').apply(lcs)
         return eigenvalues
 
