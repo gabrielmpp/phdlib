b25fe88ff544cc9ec87d1359d85f6ca32f78599c
gabriel <gabrielmpp2@gmail.com>
2019-09-02 00:36:51 +0100

    update

--- convlib/run_jasmin.py
+++ convlib/run_jasmin.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     python = '/home/users/gmpp/miniconda2/envs/phd37/bin/python'
     script_path = '/home/users/gmpp/phdlib/convlib/classifier.py'
     lcs_types = ['repelling']
-    years = range(1990, 2010)
+    years = range(2000, 2010)
     for year in years:
         for lcs_type in lcs_types:
              subprocess.call(['bsub',
@@ -14,6 +14,6 @@ if __name__ == '__main__':
              '-W','72:00',
              '-R','rusage[mem=20000]',
              '-M','20000',
-             '-n','10',
+             '-n','4',
              '-q','par-single',
              python,script_path, 'jasmin', lcs_type, str(year)])
