f6bd8f332f65784381885ecd1df874b31221bcb1
gabriel <gabrielmpp2@gmail.com>
2019-09-02 16:15:51 +0100

    update

--- convlib/plot_nc.py
+++ convlib/plot_nc.py
@@ -76,7 +76,7 @@ def plot_local():
 if __name__ == '__main__':
     # arr = read_nc_files()
     arr = xr.open_dataarray('/home/users/gmpp/out/SL_repelling_1980_1998.nc')
-    array_mean = arr.groupby('time.month').mean('time')
+    array_mean = arr.groupby('time.month').var('time')
     array_mean = xr.apply_ufunc(lambda x: np.log(x**0.5), array_mean)
     array_anomaly = xr.apply_ufunc(lambda x, y: x - y, array_mean, array_mean.mean('month'))
     max = array_anomaly.max()
