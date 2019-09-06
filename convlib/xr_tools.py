a3cf638a076f77fbfc23665e4a3978f39ee1717a
gabriel <gabrielmpp2@gmail.com>
2019-09-06 19:47:42 +0100

    update

--- convlib/xr_tools.py
+++ convlib/xr_tools.py
@@ -31,14 +31,14 @@ def get_seq_mask(ds: xr.DataArray, seq_dim: str, seq_len: int):
     :return: xr.DataArray
     """
     mask = []
-    quotient, remainder = divmod(len(ds['seq_dim'].values), seq_len)
+    quotient, remainder = divmod(len(ds[seq_dim].values), seq_len)
     assert quotient > 0, f'seq_len cannot be larger than {seq_dim} length!'
 
     if remainder != 0:
         warn(f"Length of dim {seq_dim} is not divisible by seq_len {seq_len}. Dropping last {remainder} entries.")
-        ds = ds.isel({seq_dim: slice(None, len(ds['seq_dim'].values) - remainder)})
+        ds = ds.isel({seq_dim: slice(None, len(ds[seq_dim].values) - remainder)})
 
-    for i, time in ds['seq_dim'].values:
+    for i, time in ds[seq_dim].values:
         idx = int(i/seq_len)
         mask.append(idx)
 
