25f50aa2fa8853a1843e9c7efa296bc201b14e6d
gab <gabrelmpp2@gmail.com>
2020-02-17 13:28:31 +0000

    update

--- download_era/call_download.py
+++ download_era/call_download.py
@@ -8,16 +8,16 @@ if __name__ == '__main__':
     this_file_path = Path(os.path.dirname(os.path.abspath(__file__)))
 
     logs_path = '/home/users/gmpp/logs/'
-    # python = '/home/users/gmpp/miniconda2/envs/phd37/bin/python'
-    python = '/home/gab/software/miniconda3/envs/phd37/bin/python'
+    python = '/home/users/gmpp/miniconda2/envs/phd37/bin/python'
+    # python = '/home/gab/software/miniconda3/envs/phd37/bin/python'
     # script_paths = ['/home/users/gmpp/phdlib/download_era/download_era_viwvn.py',]
     script_paths = [
                     #this_file_path / 'download_era_viwvn.py',
-                    this_file_path / 'download_era_viwve.py',
+                    this_file_path / 'download_era_convective.py',
                     #this_file_path / 'download_era_tcwv.py'
     ]
-    years = np.arange(2020, 2021, 1)
-    jasmin = False  # also change config, paths and python
+    years = np.arange(1979, 2020, 1)
+    jasmin = True  # also change config, paths and python
     if jasmin:
 
         for script_path in script_paths:
